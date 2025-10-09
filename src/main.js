8e6ed4ed01d0724363da33bec0a7f97dae60193d
RohitTiz <rohittiwari06052004@gmail.com>
2025-10-09 12:21:29 +0530

    done

--- src/main.js
+++ src/main.js
@@ -6,17 +6,5 @@ import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
   <h1> heyy<h1/>
 `
-const accountId = 144633
-let accountEmail= "rohit@gmail"
-let uiii;
-
-// never use var .. beacuse of issue in block and functional block 
-var accountPassword = "122321"
-
-
-
-accountEmail ="hui@gmail"
-accountPassword="92488r982"
-
-
-console.table([accountEmail,accountId,accountPassword,uiii])
+let huu = "ahh"
+console.log(huu);
