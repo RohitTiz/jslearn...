2f29ce816604569d5c573a85a947f59d180e6be3
RohitTiz <rohittiwari06052004@gmail.com>
2025-10-12 14:51:52 +0530

    done

--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
   <h1> heyy<h1/>
 `
-const min = 10 ;
-const max = 20 ;
-console.log(Math.floor(Math.random() *(max-min +1))+min)
+
+/// RANDOM NUMBER MATHS
+// const min = 10 ;
+// const max = 20 ;
+// console.log(Math.floor(Math.random() *(max-min +1))+min)
+//  ///////////////////////////////////////////////////////////////////////////////////////////////
+
+// let myDate = new Date();
+// console.log(myDate.toDateString)
+
+const myArray =[1,2,3,4]
+console.log(myArray[1])
