c06064e235715b465a066fa613a82a3c85765a23
RohitTiz <rohittiwari06052004@gmail.com>
2025-10-21 16:13:08 +0530

    done

--- src/main.js
+++ src/main.js
@@ -33,6 +33,29 @@ document.querySelector('#app').innerHTML = `
 
 // const {courseInst} =Course
 
-const myname ={
-  he
+//+++++++++++++++++++++++++++++++++ control flow +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const score = 200
+
+// if (score>100){
+//   const power = "fly"
+//   console.log(`user power: ${power}`);
+// }
+
+
+//implicit scope 
+const balance = 400
+// if (balance >500) console.log("test");
+
+if(balance <500){
+  console.log("less than 500")
+}
+else if(balance<750){
+  console.log("less than 750")
+}
+else if(balance<1000){
+  console.log("less than 1000")
+}
+else{
+  console.log("more than 1000")
 }
