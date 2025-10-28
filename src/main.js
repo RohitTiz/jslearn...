4f4056170b2082e6fe9967cece2129237eeeff0d
RohitTiz <rohittiwari06052004@gmail.com>
2025-10-28 16:08:16 +0530

    done

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <h1> heyy<h1/>`
+  <h1>Heyyyy</h1>`
 
-  
+const requestUrl = 'https://randomuser.me/api/'
+const xhr = new XMLHttpRequest()
+xhr.open('GET', requestUrl)
+xhr.onload = function() {
+  console.log('Response:', xhr.responseText)
+}
+xhr.send()
