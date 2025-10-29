835579c0a1aaaa8f824e019ed13f5b305e3a54e5
RohitTiz <rohittiwari06052004@gmail.com>
2025-10-29 15:08:11 +0530

    done

--- src/main.js
+++ src/main.js
@@ -7,9 +7,3 @@ document.querySelector('#app').innerHTML = `
   <h1>Heyyyy</h1>`
 
 const requestUrl = 'https://randomuser.me/api/'
-const xhr = new XMLHttpRequest()
-xhr.open('GET', requestUrl)
-xhr.onload = function() {
-  console.log('Response:', xhr.responseText)
-}
-xhr.send()
