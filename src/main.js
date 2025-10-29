8e0c1b936bc6d824557ed0fc2ece135aa24341c0
RohitTiz <rohittiwari06052004@gmail.com>
2025-10-29 15:09:17 +0530

    done

--- src/main.js
+++ src/main.js
@@ -6,10 +6,4 @@ import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
   <h1>Heyyyy</h1>`
 
-const requestUrl = 'https://randomuser.me/api/'
-const xhr = new XMLHttpRequest()
-xhr.open('GET', requestUrl)
-xhr.onload = function() {
-  console.log('Response:', xhr.responseText)
-}
-xhr.send()
+
